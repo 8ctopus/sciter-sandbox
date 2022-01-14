b17c3e416c9ed4c9c3fedcdde5cd72f1070ed44d
8ctopus <hello@octopuslabs.io>
2022-01-14 20:11:18 +0500

    fixed: install script downloads whole sciter sdk zip fixed: install script commands run synchronously

--- scripts/install.mjs
+++ scripts/install.mjs
@@ -1,44 +1,15 @@
-import https from "node:https";
 import fs from "node:fs";
 import {exec} from "node:child_process";
 import os from "node:os";
+import download from "download";
 
 // sciter 4.4.8.22-bis
 const sciterSDK = "6cedc57ff09404ad17e1899abc06f843a4677b69";
 
-function download(url, path) {
-    https.get(url, (response) => {
-        const writeStream = fs.createWriteStream(path);
-
-        response.pipe(writeStream);
-
-        writeStream.on("finish", () => {
-            writeStream.close();
-            console.log("Download Completed");
-        });
-    });
-}
-
 const commands = {
-    "win32": [
-        "mkdir bin\\windows\\x32",
-        `cd bin\\windows\\x32 & curl -LO https://github.com/c-smile/sciter-js-sdk/raw/${sciterSDK}/bin/windows/x32/scapp.exe`,
-        `cd bin\\windows\\x32 & curl -LO https://github.com/c-smile/sciter-js-sdk/raw/${sciterSDK}/bin/windows/x32/inspector.exe`,
-        `cd bin\\windows\\x32 & curl -LO https://github.com/c-smile/sciter-js-sdk/raw/${sciterSDK}/bin/windows/x32/sciter.dll`,
-    ],
     "linux": [
-        "mkdir -p bin/linux/x64",
-        `cd bin/linux/x64; curl -LO https://github.com/c-smile/sciter-js-sdk/raw/${sciterSDK}/bin/linux/x64/scapp`,
-        `cd bin/linux/x64; curl -LO https://github.com/c-smile/sciter-js-sdk/raw/${sciterSDK}/bin/linux/x64/inspector`,
-        `cd bin/linux/x64; curl -LO https://github.com/c-smile/sciter-js-sdk/raw/${sciterSDK}/bin/linux/x64/libsciter-gtk.so`,
-        "cd bin/linux/x64; chmod +x scapp inspector libsciter-gtk.so",
-    ],
-    "darwin": [
-        // download the whole archive because of inspector.app which is a directory
-        `curl -LO https://github.com/c-smile/sciter-js-sdk/archive/${sciterSDK}.zip`,
-
         // unzip binaries
-        `unzip ${sciterSDK}.zip sciter-js-sdk-${sciterSDK}/bin/macosx/* -d .`,
+        `unzip ${sciterSDK}.zip`,
 
         // move binaries
         `mv sciter-js-sdk-${sciterSDK}/bin .`,
@@ -50,25 +21,66 @@ const commands = {
         `rm ${sciterSDK}.zip`,
 
         "cd bin/macosx; chmod +x scapp inspector.app libsciter.dylib",
+        "cd bin/linux/arm32; chmod +x scapp inspector usciter libsciter-gtk.so",
+        "cd bin/linux/x64; chmod +x scapp inspector usciter libsciter-gtk.so",
+        "cd bin/linux/x32; chmod +x scapp",
+    ],
+    "win32": [
+        // unzip binaries
+        `unzip ${sciterSDK}.zip`,
+
+        // move binaries
+        `move sciter-js-sdk-${sciterSDK}\\bin .`,
+
+        // delete old dir
+        `rmdir /s /q sciter-js-sdk-${sciterSDK}`,
+
+        // delete zip
+        `del ${sciterSDK}.zip`,
     ],
 };
 
-// get operating system
-const platform = os.platform();
+console.log(`Download sciter.js SDK ${sciterSDK}...`);
 
-console.log(`Install sciter.js SDK ${sciterSDK} on ${platform}...`);
+fs.writeFileSync(`${sciterSDK}.zip`, await download(`https://github.com/c-smile/sciter-js-sdk/archive/${sciterSDK}.zip`)
+    .on("response", res => {
+        //console.log(res.headers["content-length"]);
+        console.log(res.headers);
+    })
+/*
+    .on("response", res => {
+        bar.total = res.headers["content-length"];
+        res.on("data", data => data.length);
+  })
+*/
+);
 
+
+console.log(`Install sciter.js SDK ${sciterSDK}...`);
+
+const platform = (os.platform() === "win32") ? "win32" : "linux";
+
+//console.debug(platform);
+
+// execute commands
 for (const command of commands[platform]) {
-    exec(command, (error, stdout, stderr) => {
-        if (error) {
-            console.error(error);
-            return;
-        }
+    console.log(command);
 
-        if (stdout)
-            console.log(stdout);
+    await new Promise((resolve, reject) => {
+        exec(command, (error, stdout, stderr) => {
+            if (error) {
+                console.error(error);
+                reject(error);
+                return;
+            }
 
-        if (stderr)
-            console.error(stderr);
+            if (stdout)
+                console.log(stdout);
+
+            if (stderr)
+                console.error(stderr);
+
+            resolve(stdout);
+        });
     });
 }
