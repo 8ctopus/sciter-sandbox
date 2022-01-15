0c1caf539169a5f66289ce48667c8e028bd7c850
8ctopus <hello@octopuslabs.io>
2022-01-15 19:09:53 +0500

    improved: exported start commands

---
+++ scripts/commands.mjs
@@ -0,0 +1,38 @@
+// commands for all platforms
+export const commands = {
+    inspector: {
+        win32: "./bin/windows/x32/inspector.exe",
+        linux: "./bin/linux/x64/inspector",
+        darwin: "./bin/macosx/inspector.app/Contents/MacOS/inspector",
+    },
+
+    inspector64: {
+        win32: "./bin/windows/x64/inspector.exe",
+        linux: "./bin/linux/x64/inspector",
+        darwin: "./bin/macosx/inspector.app/Contents/MacOS/inspector",
+    },
+
+    scapp: {
+        win32: "./bin/windows/x32/scapp.exe",
+        linux: "./bin/linux/x64/scapp",
+        darwin: "./bin/macosx/scapp",
+    },
+
+    scapp64: {
+        win32: "./bin/windows/x64/scapp.exe",
+        linux: "./bin/linux/x64/scapp",
+        darwin: "./bin/macosx/scapp",
+    },
+
+    usciter: {
+        win32: "./bin/windows/x32/usciter.exe",
+        linux: "./bin/linux/x64/usciter",
+        darwin: "./bin/macosx/usciter.app/Contents/MacOS/usciter",
+    },
+
+    usciter64: {
+        win32: "./bin/windows/x64/usciter.exe",
+        linux: "./bin/linux/x64/usciter",
+        darwin: "./bin/macosx/usciter.app/Contents/MacOS/usciter",
+    },
+};
--- scripts/start.mjs
+++ scripts/start.mjs
@@ -3,45 +3,7 @@ import {spawn} from "node:child_process";
 import process from "node:process";
 import {basename} from "node:path";
 import fkill from "fkill";
-
-// commands for all platforms
-const commands = {
-    inspector: {
-        win32: "./bin/windows/x32/inspector.exe",
-        linux: "./bin/linux/x64/inspector",
-        darwin: "./bin/macosx/inspector.app/Contents/MacOS/inspector",
-    },
-
-    inspector64: {
-        win32: "./bin/windows/x64/inspector.exe",
-        linux: "./bin/linux/x64/inspector",
-        darwin: "./bin/macosx/inspector.app/Contents/MacOS/inspector",
-    },
-
-    scapp: {
-        win32: "./bin/windows/x32/scapp.exe",
-        linux: "./bin/linux/x64/scapp",
-        darwin: "./bin/macosx/scapp",
-    },
-
-    scapp64: {
-        win32: "./bin/windows/x64/scapp.exe",
-        linux: "./bin/linux/x64/scapp",
-        darwin: "./bin/macosx/scapp",
-    },
-
-    usciter: {
-        win32: "./bin/windows/x32/usciter.exe",
-        linux: "./bin/linux/x64/usciter",
-        darwin: "./bin/macosx/usciter.app/Contents/MacOS/usciter",
-    },
-
-    usciter64: {
-        win32: "./bin/windows/x64/usciter.exe",
-        linux: "./bin/linux/x64/usciter",
-        darwin: "./bin/macosx/usciter.app/Contents/MacOS/usciter",
-    },
-};
+import {commands} from "./commands.mjs";
 
 // get operating system
 const platform = os.platform();
